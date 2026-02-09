59307ba1488712fe5176c088e19f8ec671729c49
sharma151 <sharmasaurav150503@gmail.com>
2026-02-09 10:50:11 +0545

    fix:Ui updated

--- src/components/UserProfileDetails.tsx
+++ src/components/UserProfileDetails.tsx
@@ -1,8 +1,7 @@
 import { HiOutlinePencilAlt } from 'react-icons/hi'
 import { FaArrowLeft } from 'react-icons/fa'
-import defaultaimage from '@/assets/default-user.webp'
 import { useAuth } from '@/core/hooks/api/useAuth'
-
+import DefaultUser from '@/components/DefaultUser'
 
 interface AllUsersListProps {
   onBack: () => void
@@ -11,35 +10,29 @@ interface AllUsersListProps {
 const UserDetailCard = ({ onBack }: AllUsersListProps) => {
   const { userdetail } = useAuth()
 
-  console.log('User Detail in Profile Card:', userdetail)
-
   return (
-    <div className="bg-[#111b21] text-[#e9edef] h-screen w-full mx-auto flex flex-col font-sans overflow-hidden">
-      <div className="flex-1 overflow-y-auto whatsapp-scrollbar relative">
-        <div className="flex items-center p-5 pt-8">
+    <div className="bg-sidebar text-[#e9edef] h-screen w-full mx-auto flex flex-col font-sans overflow-hidden">
+      {/* Scrollable container */}
+      <div className="flex-1 overflow-y-auto custom-scrollbar bg-sidebar">
+        {/* Header */}
+        <div className="flex items-center p-5 pt-8 sticky top-0 bg-sidebar z-10">
           <button
             onClick={onBack}
             className="p-2 rounded-full transition-colors cursor-pointer hover:bg-gray-800"
           >
             <FaArrowLeft size={16} />
           </button>
-          <h1 className="text-xl font-medium">Profile</h1>
+          <h1 className="text-xl font-medium ml-2">Profile</h1>
         </div>
 
-        <div className="flex flex-col items-center my-8 relative">
-          <div className="relative group mb-4">
-            <div className="w-48 h-48 rounded-full overflow-hidden border-none bg-gray-800">
-              <img
-                src={defaultaimage}
-                alt="Profile"
-                className="w-full h-full object-cover"
-              />
-            </div>
-          </div>
+        {/* Avatar */}
+        <div className="flex flex-col items-center my-8">
+          <DefaultUser size={190} iconSize={60} />
         </div>
 
-        <div className="space-y-8 p-8 pb-20">
-          {/* Name Section */}
+        {/* Content */}
+        <div className="space-y-8 px-8 pb-8">
+          {/* User name */}
           <section>
             <label className="text-[#00a884] text-sm block mb-4">
               User name
@@ -53,6 +46,7 @@ const UserDetailCard = ({ onBack }: AllUsersListProps) => {
             </div>
           </section>
 
+          {/* Full name */}
           <section>
             <label className="text-[#8696a0] text-sm block mb-4">
               Full Name
@@ -65,11 +59,12 @@ const UserDetailCard = ({ onBack }: AllUsersListProps) => {
               />
             </div>
           </section>
-          {/* About Section */}
+
+          {/* Email */}
           <section>
             <label className="text-[#8696a0] text-sm block mb-4">Email</label>
             <div className="flex items-center justify-between">
-              <span className="text-lg">
+              <span className="text-lg truncate max-w-[75%]">
                 {userdetail?.user?.email || 'Hey there! I am using WhatsApp.'}
               </span>
               <HiOutlinePencilAlt
