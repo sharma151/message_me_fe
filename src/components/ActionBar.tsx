4c5a112b72834dd7f0db7dcc3164332cd7770b38
sharma151 <sharmasaurav150503@gmail.com>
2026-02-09 15:05:41 +0545

    fix:added chaticon in action bar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,10 +1,18 @@
 import UserDetailActionButton from '@/components/UserDetailActionButton'
+import ChatIcon from '@/assets/chatIcon.png'
 
 const ActionBar = () => {
   return (
     <>
-      <div className="w-18 border  border-r shrink-0 flex flex-col  items-center justify-end pb-4 bg-gray-700 ">
-        <UserDetailActionButton />
+      <div className="w-18 border  border-r shrink-0 flex flex-col  items-center justify-between pb-4 bg-gray-700 ">
+        <div>
+          <button className="mt-5 cursor-pointer border p-2 rounded-full bg-gray-300 border-gray-500">
+            <img src={ChatIcon} alt="Chat" className="w-6 h-6" />
+          </button>
+        </div>
+        <div className="flex justify-end">
+          <UserDetailActionButton />
+        </div>
       </div>
     </>
   )
